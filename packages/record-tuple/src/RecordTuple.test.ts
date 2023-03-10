158c7e591907966073cacca87890f408fd250d6b
Patrick Wang <patrick.bohan.wang@gmail.com>
2023-03-09 17:34:48 -0800

    record-tuple: add and reorganize tests

--- packages/record-tuple/src/RecordTuple.test.ts
+++ packages/record-tuple/src/RecordTuple.test.ts
@@ -16,24 +16,29 @@ test("Creates Record or Tuple", () => {
   assert.is(RecordTuple({ a: "a", b: "b" }), RecordTuple({ a: "a", b: "b" }));
 });
 
-test(".deep: Creates deeply nested Record or Tuple from regular object", () => {
+test(".deep: Empty", () => {
+  assert.is(RecordTuple.deep([]), Tuple());
+  assert.is(RecordTuple.deep({}), Record({}));
+});
+
+test(".deep: Basic", () => {
   assert.is(RecordTuple.deep([]), Tuple());
   assert.is(RecordTuple.deep({}), Record({}));
 
   assert.is(RecordTuple.deep([1, 2, 3]), Tuple(1, 2, 3));
   assert.is(RecordTuple.deep({ a: "a", b: "b" }), Record({ a: "a", b: "b" }));
-  assert.is(
-    RecordTuple.deep({ a: "a", b: "b" }),
-    RecordTuple({ a: "a", b: "b" })
-  );
+});
 
+test(".deep: Tuple nesting", () => {
   assert.is(
     RecordTuple.deep([1, [2, 3], [4, [5, 6]]] as const),
     Tuple(1, Tuple(2, 3), Tuple(4, Tuple(5, 6)))
   );
   assert.is(RecordTuple.deep([[[[[]]]]]), Tuple(Tuple(Tuple(Tuple(Tuple())))));
   assert.is.not(RecordTuple.deep([[[]]]), Tuple(Tuple(Tuple(Tuple()))));
+});
 
+test(".deep: Record nesting", () => {
   assert.is(
     RecordTuple.deep({ a: { b: {} }, c: { d: { e: { f: "g" } } } }),
     Record({
@@ -48,7 +53,9 @@ test(".deep: Creates deeply nested Record or Tuple from regular object", () => {
       c: Record({ d: Record({ e: Record({ f: 1 }) }) }),
     })
   );
+});
 
+test(".deep: Mixed nesting", () => {
   assert.is(
     RecordTuple.deep({
       a: { b: [1, 2, 3] },
@@ -65,6 +72,22 @@ test(".deep: Creates deeply nested Record or Tuple from regular object", () => {
       { c: "c", d: "d" },
     ]),
     Tuple(Record({ a: "a", b: "b" }), Record({ c: "c", d: "d" }))
+  );
+});
+
+test(".deep: Non-serializables", () => {
+  const fn = () => {};
+  const symbol = Symbol();
+
+  assert.is(
+    RecordTuple.deep({
+      fn,
+      symbol,
+    }),
+    Record({
+      fn,
+      symbol,
+    })
   );
 });
 

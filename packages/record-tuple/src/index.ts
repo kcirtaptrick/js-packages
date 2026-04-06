67096b8c9ed4d30140d4060bd334699cee38b52a
Patrick Wang <patrick.bohan.wang@gmail.com>
2026-04-06 13:49:14 -0700

    Extract record-tuple to independent repo

--- packages/record-tuple/src/index.ts
+++
@@ -1,3 +0,0 @@
-export { default as Tuple, type Tupleable } from "./Tuple.js";
-export { default as Record, type Recordable } from "./Record.js";
-export { default as RecordTuple } from "./RecordTuple.js";
